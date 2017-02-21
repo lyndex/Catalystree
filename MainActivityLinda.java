c6f085dc8d5b7550caec96a8560a0cee49693890
lyndex <08linda18@gmail.com>
2017-02-20 17:24:56 -0800

    Create MainActivityLinda.java

---
+++ MainActivityLinda.java
@@ -0,0 +1,20 @@
+package com.example.letha.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}

08aa7a49ef5dd180d4ff258872865f20d7d1fe21
samanthahsu <sam06207891@gmail.com>
2017-02-22 21:18:16 -0800

    Update WelcomeActivity.java

--- WelcomeActivity.java
+++ WelcomeActivity.java
@@ -1,25 +1,91 @@
 package com.example.sam_2.catalystree;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends Activity
+{
+    Button btnSignIn,btnSignUp;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get The Reference Of Buttons
+        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
+        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+
+        // Set OnClick Listener on SignUp button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUP=new Intent(getApplicationContext(),SignUPActivity.class);
+                startActivity(intentSignUP);
+            }
+        });
     }
-// login button is linking to sign up for now XD
-    public void LOGIN(View view) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+    // Methods to handleClick Event of Sign In Button
+    public void signIn(View V)
+    {
+        final Dialog dialog = new Dialog(WelcomeActivity.this);
+        dialog.setContentView(R.layout.login);
+        dialog.setTitle("Login");
+
+        // get the References of views
+        final  EditText editTextUserName=(EditText)dialog.findViewById(R.id.editTextUserNameToLogin);
+        final  EditText editTextPassword=(EditText)dialog.findViewById(R.id.editTextPasswordToLogin);
+
+        Button btnSignIn=(Button)dialog.findViewById(R.id.buttonSignIn);
+
+        // Set On ClickListener
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // get The User name and Password
+                String userName=editTextUserName.getText().toString();
+                String password=editTextPassword.getText().toString();
+
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSinlgeEntry(userName);
+
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    Toast.makeText(WelcomeActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+
+                    Intent intent = new Intent(WelcomeActivity.this, ActivityHome.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(WelcomeActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        dialog.show();
     }
 
-    public void SIGNUP(View view) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
     }
 }

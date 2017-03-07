08a1cdf0b1303693eb4b0a726bda6bed7eb055ec
samanthahsu <sam06207891@gmail.com>
2017-03-07 15:42:41 -0800

    Create FragmentHome.java

---
+++ FragmentHome.java
@@ -0,0 +1,22 @@
+package com.example.catalystreeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentHome extends Fragment {
+
+    public FragmentHome() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+
+}

7b2d28b217debee93e04c9cf18039dea83062174
samanthahsu <sam06207891@gmail.com>
2017-03-07 15:42:59 -0800

    Create FragmentInput.java

---
+++ FragmentInput.java
@@ -0,0 +1,22 @@
+package com.example.catalystreeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentInput extends Fragment {
+
+    public FragmentInput() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_input, container, false);
+
+        return rootView;
+    }
+
+}

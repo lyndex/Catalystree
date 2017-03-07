3d791d2e1c44bb8e76de158883692fa846cccd21
samanthahsu <sam06207891@gmail.com>
2017-03-07 15:43:27 -0800

    Create FragmentSettings.java

---
+++ FragmentSettings.java
@@ -0,0 +1,22 @@
+package com.example.catalystreeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSettings extends Fragment {
+
+    public FragmentSettings() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return rootView;
+    }
+
+}

d37dd91dda3ebc101a1693915b0fcb55dc979b73
samanthahsu <sam06207891@gmail.com>
2017-03-07 15:43:47 -0800

    Create FragmentUsage.java

---
+++ FragmentUsage.java
@@ -0,0 +1,22 @@
+package com.example.catalystreeapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentUsage extends Fragment {
+
+    public FragmentUsage() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_usage, container, false);
+
+        return rootView;
+    }
+
+}

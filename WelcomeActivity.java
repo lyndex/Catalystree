497e17711707208e7f4ff39e0b08a9c545dc42b3
samanthahsu <sam06207891@gmail.com>
2017-03-24 11:18:29 -0700

    Update WelcomeActivity.java

--- WelcomeActivity.java
+++ WelcomeActivity.java
@@ -1,69 +1,96 @@
-package com.example.catalystreeapp.Level2Fragment;
+package com.example.catalystreeapp.Users;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+        import android.app.Activity;
+        import android.app.Dialog;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import com.example.catalystreeapp.Level3Fragment.FCar;
-import com.example.catalystreeapp.R;
+        import com.example.catalystreeapp.Main.MainActivity;
+        import com.example.catalystreeapp.R;
 
-public class FTransportation extends Fragment {
-
-    Button Bcar, Btransit, Bwalk;
+public class WelcomeActivity extends Activity
+{
+    Button btnSignIn,btnSignUp;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get The Reference Of Buttons
+        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
+        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+
+        // Set OnClick Listener on SignUp button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUp = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intentSignUp);
+            }
+        });
+    }
+    // Methods to handleClick Event of Sign In Button
+    public void signIn(View V)
+    {
+        final Dialog dialog = new Dialog(WelcomeActivity.this);
+        dialog.setContentView(R.layout.activity_sign_in);
+        dialog.setTitle("Login");
+
+        // get the References of views
+        final  EditText editTextUserName=(EditText)dialog.findViewById(R.id.editTextUserNameToLogin);
+        final  EditText editTextPassword=(EditText)dialog.findViewById(R.id.editTextPasswordToLogin);
+
+        Button btnSignIn=(Button)dialog.findViewById(R.id.buttonSignIn);
+
+        // Set On ClickListener
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // get The User name and Password
+                String userName=editTextUserName.getText().toString();
+                String password=editTextPassword.getText().toString();
+
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSingleEntry(userName);
+
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    Toast.makeText(WelcomeActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+
+                    intent.putExtra("USERNAME_KEY", userName);
+                    startActivity(intent);
+
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(WelcomeActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        dialog.show();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View myView = inflater.inflate(R.layout.fragment_transportation, container, false);
-        Bcar = (Button) myView.findViewById(R.id.b_car);
-        Bcar.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        Fragment newFragment = new FCar();
-                                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-                                        transaction.replace(R.id.fragment_car, newFragment);
-                                        transaction.addToBackStack(null);
-
-                                        transaction.commit();
-                                    }});
-//
-//            Btransit = (Button) myView.findViewById(R.id.b_transit);
-//            Btransit.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Fragment newFragment = new FCar();
-//                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//
-//                    transaction.replace(R.id.fragment_car, newFragment);
-//                    transaction.addToBackStack(null);
-//
-//                    transaction.commit();
-//                }}
-//
-//        Bwalk = (Button) myView.findViewById(R.id.b_walk);
-//        Btransit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Fragment newFragment = new FCar();
-//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//
-//                transaction.replace(R.id.fragment_car, newFragment);
-//                transaction.addToBackStack(null);
-//
-//                transaction.commit();
-//            }}
-        return myView;
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
     }
-
-
 }

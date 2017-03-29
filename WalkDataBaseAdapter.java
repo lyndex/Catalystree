7ac44c36b7efc550befa8b498a9fe68ab2a4a116
samanthahsu <sam06207891@gmail.com>
2017-03-28 18:00:20 -0700

    Create WalkDataBaseAdapter.java

---
+++ WalkDataBaseAdapter.java
@@ -0,0 +1,91 @@
+package com.example.catalystreeapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class WalkDataBaseAdapter {
+
+    private static final String DATABASE_NAME = "catalystree.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final int NAME_COLUMN = 1;
+    private static final String DATABASE_TABLE ="WALK";
+
+    public static final String COLUMN_USERNAME = "USERNAME";
+    public static final String COLUMN_DISTANCE = "DISTANCE";
+    public static final String COLUMN_TIME = "TIME";
+
+    // SQL Statement to create a new database.
+    static final String DATABASE_CREATE = "create table"+" WALK" +
+            "( " + "USERNAME text, DISTANCE int, TIME int);";
+    // Variable to hold the database instance
+    public SQLiteDatabase db;
+    // Context of the application using the database.
+    private final Context context;
+    // Database open/upgrade helper
+    private WalkDataBaseHelper dbHelper;
+
+    public WalkDataBaseAdapter(Context _context) {
+        context = _context;
+        dbHelper = new WalkDataBaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public WalkDataBaseAdapter open() throws SQLException {
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public SQLiteDatabase getDatabaseInstance() {
+        return db;
+    }
+
+    public void insertEntry(String USERNAME, Integer distance, Integer time) {
+        ContentValues newValues = new ContentValues();
+        // Assign values for each row.
+        newValues.put("USERNAME", USERNAME);
+        newValues.put("DISTANCE", distance);
+        newValues.put("TIME", time);
+
+        // Insert the row into your table
+        db.insert("CAR", null, newValues);
+        ///Toast.makeText(context, "Reminder Is Successfully Saved", Toast.LENGTH_LONG).show();
+    }
+    public int deleteEntry(String UserName) {
+        //String id=String.valueOf(ID);
+        String where = "ID=?";
+        int numberOFEntriesDeleted = db.delete("LOGIN", where, new String[]{UserName});
+        // Toast.makeText(context, "Number fo Entry Deleted Successfully : "+numberOFEntriesDeleted, Toast.LENGTH_LONG).show();
+        return numberOFEntriesDeleted;
+    }
+
+    public String getSinlgeEntry(String userName) {
+        Cursor cursor = db.query("CAR", null, " USERNAME=?", new String[]{userName}, null, null, null);
+        if (cursor.getCount() < 1) // UserName Not Exist
+        {
+            cursor.close();
+            return "NOT EXIST";
+        }
+        cursor.moveToFirst();
+        String type = cursor.getString(cursor.getColumnIndex("TYPE"));
+        cursor.close();
+        return type;
+    }
+
+    public void updateEntry(String userName, String password, String email) {
+        // Define the updated row content.
+        ContentValues updatedValues = new ContentValues();
+        // Assign values for each row.
+        updatedValues.put("USERNAME", userName);
+        updatedValues.put("PASSWORD", password);
+        updatedValues.put("Email", email);
+
+        String where = "USERNAME = ?";
+        db.update("LOGIN", updatedValues, where, new String[]{userName});
+    }
+}

03dbb58ac823ee78f9c4f32aa0907c4367d0f219
samanthahsu <sam06207891@gmail.com>
2017-04-04 18:03:17 -0700

    Update FSettings.java

--- MainFragments/FSettings.java
+++ MainFragments/FSettings.java
@@ -1,14 +1,19 @@
-package com.example.catalystreeapp;
+package com.example.catalystreeapp.MainFragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class FragmentSettings extends Fragment {
+import com.example.catalystreeapp.R;
 
-    public FragmentSettings() {
+public class FSettings extends Fragment {
+
+    public FSettings() {
     }
 
     @Override
@@ -16,7 +21,14 @@ public class FragmentSettings extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        Button button = (Button) rootView.findViewById(R.id.b_web);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://catalystree.wordpress.com/"));
+                startActivity(intent);
+            }});
         return rootView;
-    }
-
-}
+          }}

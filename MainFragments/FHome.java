a71698c4e8c3bee458985d34dddb883d57648fcc
samanthahsu <sam06207891@gmail.com>
2017-04-18 21:59:03 -0700

    Update FHome.java

--- MainFragments/FHome.java
+++ MainFragments/FHome.java
@@ -1,14 +1,20 @@
-package com.example.catalystreeapp;
+package com.example.catalystreeapp.MainFragments;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class FragmentHome extends Fragment {
+import com.example.catalystreeapp.R;
 
-    public FragmentHome() {
+public class FHome extends Fragment {
+
+    AnimationDrawable neutral_idle;
+
+    public FHome() {
     }
 
     @Override
@@ -16,7 +22,11 @@ public class FragmentHome extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        ImageView pandaImage = (ImageView) rootView.findViewById(R.id.pandaView);
+        pandaImage.setBackgroundResource(R.drawable.neutral_idle);
+        neutral_idle = (AnimationDrawable) pandaImage.getBackground();
+        neutral_idle.start();
+
         return rootView;
     }
-
 }

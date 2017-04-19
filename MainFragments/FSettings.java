a992ec1b9df99129a1bce7ec950ae58ec0efe577
samanthahsu <sam06207891@gmail.com>
2017-04-18 21:59:44 -0700

    Update FSettings.java

--- MainFragments/FSettings.java
+++ MainFragments/FSettings.java
@@ -1,25 +1,41 @@
 package com.example.catalystreeapp.MainFragments;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.catalystreeapp.R;
+import com.example.catalystreeapp.Users.SessionManagement;
+import com.example.catalystreeapp.Users.WelcomeActivity;
 
 public class FSettings extends Fragment {
 
+    AnimationDrawable pandarun;
+    SessionManagement session;
+    Button btnLogout;
+
     public FSettings() {
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        session = new SessionManagement(getActivity().getApplicationContext());
+
+        ImageView pandaImage = (ImageView) rootView.findViewById(R.id.panda_run);
+        pandaImage.setBackgroundResource(R.drawable.pandarun);
+        pandarun = (AnimationDrawable) pandaImage.getBackground();
+        pandarun.start();
 
         Button button = (Button) rootView.findViewById(R.id.b_web);
         button.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +46,33 @@ public class FSettings extends Fragment {
                 intent.setData(Uri.parse("https://catalystree.wordpress.com/"));
                 startActivity(intent);
             }});
+        // Button logout
+        btnLogout = (Button) rootView.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("!");
+                builder.setMessage("Do you want to logout?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//              clear shared pref
+                        session.logoutUser();
+                    }
+                });
+                builder.setNegativeButton("Not now", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
         return rootView;
-          }}
+          }
+}

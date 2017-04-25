f1b08f43c439562ed4cea32bc7afe3dd8ed8a2e4
samanthahsu <sam06207891@gmail.com>
2017-04-24 23:11:42 -0700

    Update FHome.java

--- MainFragments/FHome.java
+++ MainFragments/FHome.java
@@ -1,6 +1,7 @@
 package com.example.catalystreeapp.MainFragments;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,8 +13,8 @@ import com.example.catalystreeapp.R;
 
 public class FHome extends Fragment {
 
-    AnimationDrawable neutral_idle;
-
+    AnimationDrawable neutral_idle, grump_idle, mad_idle, happy_idle;
+    int[] Energy;
     public FHome() {
     }
 
@@ -21,12 +22,39 @@ public class FHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
         ImageView pandaImage = (ImageView) rootView.findViewById(R.id.pandaView);
-        pandaImage.setBackgroundResource(R.drawable.neutral_idle);
-        neutral_idle = (AnimationDrawable) pandaImage.getBackground();
-        neutral_idle.start();
 
+//        SOUND WHEN PANDA TAPPED
+        final MediaPlayer mp = MediaPlayer.create(getActivity(), R.raw.chirp);
+        pandaImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
+        int j = FUsageEnergy.energySum;
+        int Today = FUsageEnergy.TODAYENERGY;
+        Double Previous = (double) (j / 7);
+        Double percentchange = (Today / Previous) * 100;
+
+        if(percentchange < 75) {
+            pandaImage.setBackgroundResource(R.drawable.happy_idle);
+            happy_idle = (AnimationDrawable) pandaImage.getBackground();
+            happy_idle.start();
+        } else if(percentchange < 110 && percentchange >= 75) {
+            pandaImage.setBackgroundResource(R.drawable.neutral_idle);
+            neutral_idle = (AnimationDrawable) pandaImage.getBackground();
+            neutral_idle.start();
+        } else if (percentchange < 150 && percentchange >= 110) {
+            pandaImage.setBackgroundResource(R.drawable.grump_idle);
+            grump_idle = (AnimationDrawable) pandaImage.getBackground();
+            grump_idle.start();
+        } else {
+            pandaImage.setBackgroundResource(R.drawable.mad_idle);
+            mad_idle = (AnimationDrawable) pandaImage.getBackground();
+            mad_idle.start();
+        }
         return rootView;
     }
 }
